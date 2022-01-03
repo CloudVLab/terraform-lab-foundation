872cc4a25dd0a75fb6ab82964917f24ef658d2c9
rosera <askrichardrose@gmail.com>
2022-01-03 20:23:28 +0000

    Add: Firewall rules module - handle objects

--- basics/vpc_firewall/stable/variables.tf
+++ basics/vpc_firewall/stable/variables.tf
@@ -29,6 +29,33 @@ variable "gcp_zone" {
 ## Custom variable definitions
 ## --------------------------------------------------------------
 
+variable "fwr_rules" {
+  description = "List of custom rule definitions (refer to variables file for syntax)."
+  default     = []
+  type = list(object({
+    fwr_name                    = string
+    fwr_description             = string
+    fwr_direction               = string
+    fwr_priority                = number
+    fwr_source_ranges                  = list(string)
+    fwr_source_tags             = list(string)
+    fwr_source_service_accounts = list(string)
+    fwr_target_tags             = list(string)
+    fwr_target_service_accounts = list(string)
+    allow = list(object({
+      protocol = string
+      ports    = list(string)
+    }))
+    deny = list(object({
+      protocol = string
+      ports    = list(string)
+    }))
+    log_config = object({
+      metadata = string
+    })
+  }))
+}
+
 # Default value passed in
 variable "fwr_name" {
   type        = string
