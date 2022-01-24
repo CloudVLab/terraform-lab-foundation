d92d7f26796f01347db5d99ab921198678024444
Rich Rose <askrichardrose@gmail.com>
2022-01-24 18:52:43 +0000

    Update outputs.tf
    Add output variables to include in the qwiklabs panel.

--- solutions/appsheet_cloudsql/example/outputs.tf
+++ solutions/appsheet_cloudsql/example/outputs.tf
@@ -7,3 +7,14 @@
 #   description = "Cloud SQL Instance"
 # }
 
+output "cloud_sql_private_ip" {
+  sensitive = false
+  value = module.la_cloudsql.cloud_sql_private_ip
+  description = "Cloud SQL Private IP"
+}
+
+output "cloud_sql_public_ip" {
+  sensitive = false
+  value = module.la_cloudsql.cloud_sql_public_ip
+  description = "Cloud SQL Public IP"
+}
