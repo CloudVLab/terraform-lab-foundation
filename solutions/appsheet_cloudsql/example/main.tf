40e4d59165b6d4384cae78f3a9f3e771ddaabbf3
rosera <askrichardrose@gmail.com>
2022-02-10 10:40:41 +0000

    Add: example VM database configuration

--- solutions/appsheet_cloudsql/example/main.tf
+++ solutions/appsheet_cloudsql/example/main.tf
@@ -1,4 +1,24 @@
 # Module: Google CloudSQL 
+module "la_gce" {
+  source = "github.com/CloudVLab/terraform-lab-foundation//basics/gce_instance/stable"
+
+  # Pass values to the module
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = var.gcp_region
+  gcp_zone       = var.gcp_zone
+
+  # Customise the GCE instance
+  gce_name            = "mysql-admin"
+  gce_tags            = ["lab-vm"]
+  gce_machine_image   = "debian-cloud/debian-10"
+  gce_machine_network = "default"
+  gce_scopes          = ["cloud-platform"]
+  gce_startup_script  = "${file("./scripts/lab-init")}"
+
+  # Create the CloudSQL instance
+  depends_on = [ module.la_cloudsql]
+}
+
 module "la_cloudsql" {
   ## NOTE: When changing the source parameter, `terraform init` is required
 
