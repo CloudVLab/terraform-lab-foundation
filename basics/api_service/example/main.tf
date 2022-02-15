35d68187afe15a12726da97df465c2d6822a9f31
rosera <askrichardrose@gmail.com>
2022-02-15 11:36:51 +0000

    Add: API service example

---
+++ basics/api_service/example/main.tf
@@ -0,0 +1,16 @@
+# API Service:   Enable GoogleAPIs 
+# Local:  modules/[channel]
+# Remote: github.com://CloudVLab/terraform-lab-foundation//[module]/[channel]
+
+# Module: Google Compute Engine
+module "la_api_run" {
+  source = "github.com/CloudVLab/terraform-lab-foundation//basics/api_service/stable"
+
+  # Pass values to the module
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = var.gcp_region
+  gcp_zone       = var.gcp_zone
+
+  # Customise the GCE instance
+  api_service_name = "run.googleapis.com"  
+}
