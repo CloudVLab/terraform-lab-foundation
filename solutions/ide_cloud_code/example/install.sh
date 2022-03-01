722c1fbd8c900410aacc05a09bd59b5a176df857
rosera <askrichardrose@gmail.com>
2022-03-01 07:10:59 +0000

    Add: bash variables and item checking

--- solutions/ide_cloud_code/example/install.sh
+++ solutions/ide_cloud_code/example/install.sh
@@ -1,5 +1,36 @@
 #!/bin/sh
-curl -LO https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/main.tf  -o main.tf
-curl -LO https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/outputs.tf  -o outputs.tf
-curl -LO https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/runtime.yaml  -o runtime.yaml
-curl -LO https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/variables.tf  -o variables.tf
+
+DIRECTORY="tf"
+FILE1="main.tf"
+FILE1_URL="https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/main.tf"
+FILE2="outputs.tf"
+FILE2_URL="https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/outputs.tf"
+FILE3="runtime.yaml"
+FILE3_URL="https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/runtime.yaml"
+FILE4="variables.tf"
+FILE4_URL="https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/variables.tf"
+
+# Create TF directory if not present
+if [ ! -d $DIRECTORY ]; then
+  mkdir $DIRECTORY 
+fi
+
+# Download if the file does not exist
+if [ ! -f $DIRECTORY/$FILE1 ]; then
+curl -LO $FILE1_URL -o $FILE1
+fi 
+
+# Download if the file does not exist
+if [ ! -f $DIRECTORY/$FILE2 ]; then
+curl -LO $FILE2_URL -o $FILE2
+fi
+
+# Download if the file does not exist
+if [ ! -f $DIRECTORY/$FILE3 ]; then
+curl -LO $FILE3_URL -o $FILE3
+fi
+
+# Download if the file does not exist
+if [ ! -f $DIRECTORY/$FILE4 ]; then
+curl -LO $FILE4_URL -o $FILE4
+fi
