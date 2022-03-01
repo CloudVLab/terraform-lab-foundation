eb6dc6c11166fa973e8c4e93c6fe811ccb49d5c2
rosera <askrichardrose@gmail.com>
2022-03-01 06:30:46 +0000

    Add: Easy example installation script

---
+++ solutions/ide_cloud_code/example/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -LO https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/main.tf  -o main.tf
+curl -LO https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/outputs.tf  -o outputs.tf
+curl -LO https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/runtime.yaml  -o runtime.yaml
+curl -LO https://github.com/CloudVLab/terraform-lab-foundation/raw/main/solutions/ide_cloud_code/example/variables.tf  -o variables.tf
