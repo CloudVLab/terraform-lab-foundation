1f7c8bd117623ae8fbbd380020159c8d641f332e
rosera <askrichardrose@gmail.com>
2022-03-22 16:17:11 +0000

    Add: change to output variables

--- solutions/proxy_vertex_workbench/dev/outputs.tf
+++ solutions/proxy_vertex_workbench/dev/outputs.tf
@@ -7,7 +7,7 @@
 ##   description = "Notebook instance proxy URI"
 ## }
 
-output "notebook_url" {
+output "notebook_service_url" {
   value       = "${google_cloud_run_service.jupyter.status[0].url}"
   description = "URL of the Proxy service"
 }
--- solutions/proxy_vertex_workbench/example/outputs.tf
+++ solutions/proxy_vertex_workbench/example/outputs.tf
@@ -3,6 +3,6 @@
 ## --------------------------------------------------------------
 
 output "notebook_service_url" {
-  value       = module.la_vertex_workbench.notebook_url
+  value       = module.la_vertex_workbench.notebook_service_url
   description = "URL of the Vertex notebook service"
 }
