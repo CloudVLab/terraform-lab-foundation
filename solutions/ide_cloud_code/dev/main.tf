62738198df9e4ebdac18902ddc2e23929ceee3c8
rosera <askrichardrose@gmail.com>
2022-04-05 19:15:22 +0100

    Add: null definition for false value

--- solutions/ide_cloud_code/dev/main.tf
+++ solutions/ide_cloud_code/dev/main.tf
@@ -286,14 +286,14 @@ resource "google_container_cluster" "primary" {
   initial_node_count = var.gkeIsAutopilot ? null : var.gkeInitialNodeCount
 
   # Condition setting to variable. If defined set to variable, otherwise default to false 
-  enable_binary_authorization = var.gkeIsBinAuth ? var.gkeIsBinAuth : false
+  enable_binary_authorization = var.gkeIsBinAuth ? var.gkeIsBinAuth : null 
 
   # Condition setting to variable. If defined set to variable, default to false
-  enable_autopilot            = var.gkeIsAutopilot ? var.gkeIsAutopilot : false 
+  enable_autopilot            = var.gkeIsAutopilot ? var.gkeIsAutopilot : null 
 
   private_cluster_config {
-    enable_private_endpoint = var.gkeIsPrivateEndpoint 
-    enable_private_nodes    = var.gkeIsPrivateCluster ? true : false
+    enable_private_endpoint = var.gkeIsPrivateCluster ? true : null
+    enable_private_nodes    = var.gkeIsPrivateCluster ? true : null 
     master_ipv4_cidr_block  = var.gkeIsPrivateCluster ? var.gkeMasterIPv4CIDRBlock : null
   }
     
