83ddfc93dbbf36e1f742baf0d56aa22b541249af
rosera <askrichardrose@gmail.com>
2022-04-05 15:18:35 +0100

    Add: better base config

--- solutions/ide_cloud_code/example/outputs.tf
+++ solutions/ide_cloud_code/example/outputs.tf
@@ -7,10 +7,6 @@ output "service_url" {
   description = "URL of the IDE service"
 }
 
-output "live_server_url" {
-  value       = module.la_ide.ideBrowserService
-  description = "URL of the Browser service"
-}
 
 output "resource_instance_name" {
   value       = module.la_ide.ideInstanceName
