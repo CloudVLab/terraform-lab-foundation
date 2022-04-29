6ef62322b23ce5ac7c692e0819618d51a19d6343
rosera <askrichardrose@gmail.com>
2022-04-29 14:06:20 +0100

    Add: Use GCP_REGION/ZONE

--- solutions/ide_gke_developer/dev/main.tf
+++ solutions/ide_gke_developer/dev/main.tf
@@ -161,7 +161,7 @@ resource "google_project_service" "run" {
 
 resource "google_cloud_run_service" "ide" {
   name     = var.gcrIDEService
-  location = var.gcrRegion
+  location = var.gcp_region
 
   template {
     spec {
