0ccb97522d76536c6b6c3e089ba8ebcecda54400
rosera <askrichardrose@gmail.com>
2022-05-25 14:08:55 +0100

    Add: variable definition

--- basics/vai_notebook/example/variables.tf
+++ basics/vai_notebook/example/variables.tf
@@ -17,13 +17,3 @@ variable "gcp_zone" {
 # ------------------  Custom Properties
 #
 
-variable "tfResourceName" {
-  type        = string
-  description = "The name of resource to be created"
-}
-
-variable "tfUsername" {
-  type        = string
-  description = "The name assigned to the project"
-}
-
