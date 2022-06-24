48b6f4c1b598828b9cdd1774c578b530c5b9d103
rosera <askrichardrose@gmail.com>
2022-06-24 08:13:19 +0100

    Add: output the object to test

--- basics/secret_manager/stable/outputs.tf
+++ basics/secret_manager/stable/outputs.tf
@@ -1,7 +1,12 @@
 ## --------------------------------------------------------------
 ## Custom variable defintions
 ## --------------------------------------------------------------
+## output "secret_id" {
+##   value       = "${google_secret_manager_secret.lab-secret.id}"
+##   description = "Secret manager identifier"
+## }
+
+
 output "secret_id" {
-  value       = "${google_secret_manager_secret.lab-secret.id}"
-  description = "Secret manager identifier"
+  value = "google_secret_manager_secret.lab-secret"
 }
