88927a5f68760cded46cf1d89ae89e071ef27594
rosera <askrichardrose@gmail.com>
2022-07-15 21:10:41 +0100

    Update: data properties

--- basics/lab_project/stable/outputs.tf
+++ basics/lab_project/stable/outputs.tf
@@ -2,15 +2,15 @@
 ## Custom variable definitions
 ## --------------------------------------------------------------
 
-## output "gcp_project_id" {
-##   value       = "${var.gcp_project_id}"
-##   description = "Project Identier"
-## }
-## 
-## output "gcp_project_number" {
-##   value       = data.google_project.project.number 
-##   description = "Project Identifier"
-## }
+output "gcp_project_id" {
+  value       = "${var.gcp_project_id}"
+  description = "Project Identier"
+}
+
+output "gcp_project_number" {
+  value       = data.google_project.project.number 
+  description = "Project Identifier"
+}
 
 output "gcp_project_region" {
   value       = "${var.gcp_region}"
