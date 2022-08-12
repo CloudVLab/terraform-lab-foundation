122d2cf75235e86a4498c7b40ddabe34181cae71
rosera <askrichardrose@gmail.com>
2022-08-12 11:38:27 +0100

    Add: Example output variable

--- solutions/proxy_automl/example/outputs.tf
+++ solutions/proxy_automl/example/outputs.tf
@@ -1,3 +1,6 @@
 ## --------------------------------------------------------------
 ## Custom variable defintions
 ## --------------------------------------------------------------
+output "service_url" {
+    value = module.la_automl.gcr_service_url
+}
