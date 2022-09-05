97dec6e935676a6cae3f106e0375f46f674ca175
rosera <askrichardrose@gmail.com>
2022-09-05 12:07:59 +0100

    Add: Non authorative default for SA role

--- basics/vai_notebook/stable/main.tf
+++ basics/vai_notebook/stable/main.tf
@@ -35,10 +35,18 @@ resource "google_service_account" "service_account" {
 
 # Reference
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
-#
-resource "google_project_iam_binding" "vertex_viewer_bind" {
+# Authorative for a given role.
+## resource "google_project_iam_binding" "vertex_viewer_bind" {
+##   role    = "roles/viewer"
+##   project = var.gcp_project_id
+##   members = [
+##     "serviceAccount:vertex-ai@${var.gcp_project_id}.iam.gserviceaccount.com",
+##   ]
+##   depends_on = [google_service_account.service_account]
+## }
+
+resource "google_project_iam_member" "vertex_viewer_bind" {
   role    = "roles/viewer"
-  # role    = "roles/owner"
   project = var.gcp_project_id
   members = [
     "serviceAccount:vertex-ai@${var.gcp_project_id}.iam.gserviceaccount.com",
