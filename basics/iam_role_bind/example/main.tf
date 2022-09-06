d2ea759509c36a12235eb04d7772c6c14c84c5d2
rosera <askrichardrose@gmail.com>
2022-09-06 08:40:52 +0100

    Add: folder change

--- basics/iam_role_bind/example/main.tf
+++ basics/iam_role_bind/example/main.tf
@@ -9,13 +9,13 @@ module "la_role_bind" {
   ## https://www.terraform.io/language/modules/sources#selecting-a-revision
   ## Local Modules - working
   ## Module subdirectory needs to be defined within the TF directory
-  ## source = "./basics/vai_notebook/stable"
+  ## source = "./basics/iam_role_bind/stable"
 
   ## REMOTE: GitHub (Public) access - working 
-  source = "github.com/CloudVLab/terraform-lab-foundation//basics/vai_notebook/dev?ref=tlf_iam"
+  source = "github.com/CloudVLab/terraform-lab-foundation//basics/iam_role_bind/dev?ref=tlf_iam"
 
   ## REMOTE: GitHub (Public) access - working 
-  ## source = "github.com/CloudVLab/terraform-lab-foundation//basics/vai_notebook/stable"
+  ## source = "github.com/CloudVLab/terraform-lab-foundation//basics/iam_role_bind/stable"
 
   ## Exchange values between Qwiklabs and Module
   gcp_project_id = var.gcp_project_id
