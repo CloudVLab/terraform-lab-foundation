3fb95ab1faa639d343f9dafc539095eeb5da5dc1
rosera <askrichardrose@gmail.com>
2022-09-16 18:43:34 +0100

    Fix: Example output variable reference

--- basics/bq_dataset/example/outputs.tf
+++ basics/bq_dataset/example/outputs.tf
@@ -2,7 +2,7 @@
 
 # Terraform Output values
 output "bq_dataset_id" {
-  value       = google_bigquery_dataset.default.dataset_id
+  value       = module.la_bq_dataset.bq_dataset_id
   description = "Name of the BigQuery dataset"
 }
 
