be081e6ba9a95374b03a82edd371d0cd59dcb8d7
achyut-sskills <achyut.sharma@sureskills.com>
2022-10-11 18:52:20 +0530

    Debug apigee tf module

---
+++ basics/apigee_organisation/example/main.tf
@@ -0,0 +1,10 @@
+# Module: Apigee Organisation
+module "apigee" {
+  source   = "github.com/SureSkills/terraform-lab-foundation//basics/apigee_organisation/stable"
+  # source = "./custom/apigee/stable"
+
+  # Pass values to the module
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = var.gcp_region
+  gcp_zone       = var.gcp_zone
+}
