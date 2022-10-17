e599792606392911d81f82048e71a490f1a5c500
Rich Rose <askrichardrose@gmail.com>
2022-10-17 16:56:28 +0100

    Update main.tf
    Update example script.

--- basics/cloud_run/example/main.tf
+++ basics/cloud_run/example/main.tf
@@ -3,14 +3,14 @@
 # Remote: github.com://CloudVLab/terraform-lab-foundation//[module]/[channel]
 
 # Module: AutoML  
-module "la_automl" {
-  source = "github.com/CloudVLab/terraform-lab-foundation//basics/gce_instance/stable?ref=tlf-basics"
+module "la_cloud_run" {
+  #source = "github.com/CloudVLab/terraform-lab-foundation//basics/gce_instance/stable?ref=tlf-basics"
+  source = "github.com/CloudVLab/terraform-lab-foundation//basics/gce_instance/stable"
 
   # Pass values to the module
   gcp_project_id = var.gcp_project_id
   gcp_region     = var.gcp_region
   gcp_zone       = var.gcp_zone
-  gcp_username   = var.tfUsername
 
   # Customise the GCE instance
   #gcrService = "automl-proxy"
