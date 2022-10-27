5ea3eec8cecb5901361bc17c06cea4b52b3f195c
rosera <askrichardrose@gmail.com>
2022-10-27 17:41:33 +0100

    Add: module output variables

--- solutions/cloud_code_developer/stable/outputs.tf
+++ solutions/cloud_code_developer/stable/outputs.tf
@@ -2,12 +2,14 @@
 ## Custom variable defintions
 ## --------------------------------------------------------------
 
+
 output "ideEditorService" {
-  value       = "${google_cloud_run_service.ide.status[0].url}"
+  value       = "module.la_ide_proxy.ideEditorService"
   description = "URL of the IDE service"
 }
 
+
 output "ideInstanceName" {
-  value       = "${var.gceInstanceName}"
+  value       = "module.la_gce.gce_instance_name"
   description = "Name of the GCE instance"
 }
