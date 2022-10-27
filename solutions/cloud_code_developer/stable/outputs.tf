aa79e00e8519947fd6906a5feb7ab008e40f42a0
rosera <askrichardrose@gmail.com>
2022-10-27 18:48:29 +0100

    Add: vpc connector dependency + output variable fix

--- solutions/cloud_code_developer/stable/outputs.tf
+++ solutions/cloud_code_developer/stable/outputs.tf
@@ -4,12 +4,12 @@
 
 
 output "ideEditorService" {
-  value       = "module.la_ide_proxy.ideEditorService"
+  value       = "${module.la_ide_proxy.ideEditorService}"
   description = "URL of the IDE service"
 }
 
 
 output "ideInstanceName" {
-  value       = "module.la_gce.gce_instance_name"
+  value       = "${module.la_gce.gce_instance_name}"
   description = "Name of the GCE instance"
 }
