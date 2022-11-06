0fe78f6ed0dcd08350dbca182b8d93d6eb48b08e
rosera <askrichardrose@gmail.com>
2022-11-06 21:09:51 +0000

    Add: Example cf demo

--- basics/cloud_function/example/main.tf
+++ basics/cloud_function/example/main.tf
@@ -7,16 +7,18 @@ module "la_gcf" {
   source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_function/stable"
 
   # Pass values to the module
-  gcp_project_id = var.gcp_project_id
-  gcp_region     = var.gcp_region
-  gcp_zone       = var.gcp_zone
+  gcp_project_id       = var.gcp_project_id
+  gcp_region           = var.gcp_region
+  gcp_zone             = var.gcp_zone
+
+  # Customise the Cloud Storage
+  gcs_bucket_extension = "bucket"
 
   # Customise the Cloud Function
-  gcf_name          = "myfunction"
-  gcf_description   = "Test Cloud Function"
-  gcf_runtime       = "nodejs16"
-  gcf_target_bucket = "mybucket"
-  gcf_local_source  = "./cf/function.zip"
-  gcf_entry_point   = "httpHello"
-  
+  gcf_name             = "myfunction"
+  gcf_description      = "Test Cloud Function"
+  gcf_runtime          = "nodejs16"
+  gcf_target_bucket    = "mybucket"
+  gcf_local_source     = "./cf/function.zip"
+  gcf_entry_point      = "httpHello"
 }
