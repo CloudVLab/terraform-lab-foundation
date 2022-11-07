a6df00efeb8df504036b129a004cc351514967ba
rosera <askrichardrose@gmail.com>
2022-11-07 15:35:33 +0000

    Fix: Remove CF to isolated module

--- solutions/dialogflow_webhook/stable/main.tf
+++ solutions/dialogflow_webhook/stable/main.tf
@@ -151,44 +151,44 @@ module "la_firestore_650" {
 ##------------------------------------------------------------
 #
 
-# Module: Cloud Functions
-module "la_gcf_mp" {
-  source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_function/stable"
-
-  # Pass values to the module
-  gcp_project_id = var.gcp_project_id
-  gcp_region     = var.gcp_region
-  gcp_zone       = var.gcp_zone
-
-  # Customise the Cloud Storage
-  gcs_bucket_extension = "mp"
-
-  # Customise the Cloud Function
-  gcf_name             = "cloudioGetMostPlayed"
-  gcf_entry_point      = "mostPlayed"
-  gcf_description      = "Most Played Cloud Function"
-  gcf_runtime          = "nodejs16"
-  gcf_archive_object   = "mostplayed.zip"
-  gcf_archive_source   = "./cf/mostplayed.zip"
-}
-
-# Module: Cloud Functions
-module "la_gcf_ac" {
-  source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_function/stable"
-
-  # Pass values to the module
-  gcp_project_id = var.gcp_project_id
-  gcp_region     = var.gcp_region
-  gcp_zone       = var.gcp_zone
-
-  # Customise the Cloud Storage
-  gcs_bucket_extension = "ac"
-
-  # Customise the Cloud Function
-  gcf_name             = "cloudioAccountUpdate"
-  gcf_entry_point      = "accountUpdate"
-  gcf_description      = "Accounts Cloud Function"
-  gcf_runtime          = "nodejs16"
-  gcf_archive_object   = "account.zip"
-  gcf_archive_source   = "./cf/account.zip"
-}
+## # Module: Cloud Functions
+## module "la_gcf_mp" {
+##   source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_function/stable"
+## 
+##   # Pass values to the module
+##   gcp_project_id = var.gcp_project_id
+##   gcp_region     = var.gcp_region
+##   gcp_zone       = var.gcp_zone
+## 
+##   # Customise the Cloud Storage
+##   gcs_bucket_extension = "mp"
+## 
+##   # Customise the Cloud Function
+##   gcf_name             = "cloudioGetMostPlayed"
+##   gcf_entry_point      = "mostPlayed"
+##   gcf_description      = "Most Played Cloud Function"
+##   gcf_runtime          = "nodejs16"
+##   gcf_archive_object   = "mostplayed.zip"
+##   gcf_archive_source   = "./cf/mostplayed.zip"
+## }
+## 
+## # Module: Cloud Functions
+## module "la_gcf_ac" {
+##   source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_function/stable"
+## 
+##   # Pass values to the module
+##   gcp_project_id = var.gcp_project_id
+##   gcp_region     = var.gcp_region
+##   gcp_zone       = var.gcp_zone
+## 
+##   # Customise the Cloud Storage
+##   gcs_bucket_extension = "ac"
+## 
+##   # Customise the Cloud Function
+##   gcf_name             = "cloudioAccountUpdate"
+##   gcf_entry_point      = "accountUpdate"
+##   gcf_description      = "Accounts Cloud Function"
+##   gcf_runtime          = "nodejs16"
+##   gcf_archive_object   = "account.zip"
+##   gcf_archive_source   = "./cf/account.zip"
+## }
