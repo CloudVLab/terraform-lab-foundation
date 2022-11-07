b137271bfca0a1e8a4b1d6e8bedea207b9d3448f
rosera <askrichardrose@gmail.com>
2022-11-07 16:02:42 +0000

    Fix: Remove Cloud Function module

--- solutions/dialogflow_webhook/stable/main.tf
+++ solutions/dialogflow_webhook/stable/main.tf
@@ -151,26 +151,26 @@ module "la_firestore_650" {
 ##------------------------------------------------------------
 #
 
-# Module: Cloud Functions
-module "la_gcf_mp" {
-  source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_function/stable"
-
-  # Pass values to the module
-  gcp_project_id = var.gcp_project_id
-  gcp_region     = var.gcp_region
-  gcp_zone       = var.gcp_zone
-
-  # Customise the Cloud Storage
-  gcs_bucket_extension = "mp"
-
-  # Customise the Cloud Function
-  gcf_name             = "cloudioGetMostPlayed"
-  gcf_entry_point      = "mostPlayed"
-  gcf_description      = "Most Played Cloud Function"
-  gcf_runtime          = "nodejs16"
-  gcf_archive_object   = var.function_1_zip 
-  gcf_archive_source   = var.function_1_source
-}
+## # Module: Cloud Functions
+## module "la_gcf_mp" {
+##   source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_function/stable"
+## 
+##   # Pass values to the module
+##   gcp_project_id = var.gcp_project_id
+##   gcp_region     = var.gcp_region
+##   gcp_zone       = var.gcp_zone
+## 
+##   # Customise the Cloud Storage
+##   gcs_bucket_extension = "mp"
+## 
+##   # Customise the Cloud Function
+##   gcf_name             = "cloudioGetMostPlayed"
+##   gcf_entry_point      = "mostPlayed"
+##   gcf_description      = "Most Played Cloud Function"
+##   gcf_runtime          = "nodejs16"
+##   gcf_archive_object   = var.function_1_zip 
+##   gcf_archive_source   = var.function_1_source
+## }
 
 ## # Module: Cloud Functions
 ## module "la_gcf_ac" {
