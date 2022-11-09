329f244375c7db091e4cdc21a9d83691a2c74f7b
mahakverma-tudip <vmahak@google.com>
2022-11-09 20:18:36 +0530

    - Updated source destination in main.tf - Removed commented out code from main.tf and outputs.tf

--- solutions/apigee/example/main.tf
+++ solutions/apigee/example/main.tf
@@ -1,17 +1,5 @@
-# module "apigee_org" {
-#   # source = "github.com/SureSkills/terraform-lab-foundation//basics/apigee_organisation/stable"
-#   source = "./solutions//apigee/dev"
-
-#   # Pass values to the module
-#   gcp_project_id = var.gcp_project_id
-#   gcp_region     = var.gcp_region
-#   gcp_zone       = var.gcp_zone
-
-# }
-
 module "gce_instance" {
-  source = "./custom/gce_instance/stable"
-  # source = "github.com/SureSkills/terraform-lab-foundation//basics/apigee_organisation/stable"
+  source = "github.com/CloudVLab/terraform-lab-foundation//solutions/apigee/dev"
 
   # Pass values to the module
   gcp_project_id = var.gcp_project_id
--- solutions/apigee/example/outputs.tf
+++
@@ -1,7 +0,0 @@
-# output "region_name" {
-#   value = var.region_name
-# }
-
-# output "apigee_network" {
-#   value = var.apigee_name
-# }
