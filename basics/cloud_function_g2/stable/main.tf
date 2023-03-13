404cc948d5705a1de85c67b113d31aa237b05100
rosera <askrichardrose@gmail.com>
2023-03-13 19:34:14 +0000

    Add: Initial version

---
+++ basics/cloud_function_g2/stable/main.tf
@@ -0,0 +1,70 @@
+#
+## Cloud Storage 
+#-----------------------------------------------------------------------------
+#
+### CALL: Cloud Storage Module
+
+resource "google_storage_bucket" "bucket" {
+  name                        = "${var.gcp_project_id}-${var.gcs_bucket_extension}"
+  location                    = var.gcp_region
+  project                     = var.gcp_project_id
+  force_destroy               = true
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = var.gcf_archive_object 
+  #name   = "mostplayed.zip" 
+  bucket = google_storage_bucket.bucket.name
+  source = var.gcf_archive_source 
+  #source = "./cf/mostplayed.zip" 
+
+  depends_on = [ google_storage_bucket.bucket ]
+}
+
+#
+## Cloud Functions 
+#-----------------------------------------------------------------------------
+#
+## NEW Module: Cloud Function
+
+resource "google_cloudfunctions_function" "custom_function" {
+  name                  = var.gcf_name 
+  project               = var.gcp_project_id
+  location              = var.gcp_region
+  description           = var.gcf_description 
+
+  available_memory_mb   = 128
+  build_config {
+    runtime               = var.gcf_runtime
+    entry_point           = var.gcf_entry_point 
+    source {
+      bucket = google_storage_bucket.bucket.name
+      object = google_storage_bucket_object.archive.name
+    }
+  }
+
+  service_config {
+    max_instance_count    = 1
+    available_memory      = "256M"
+    timeout_seconds       = 60
+  }
+
+  depends_on = [ google_storage_bucket_object.archive ]
+}
+
+#
+## IAM Cloud Functions
+#-----------------------------------------------------------------------------
+#
+## CALL IAM to apply role to member
+## CALL: IAM Module 
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions2_function_iam_member" "invoker" {
+  project        = var.gcp_project_id
+  location       = var.gcp_region
+  cloud_function = google_cloudfunctions_function.custom_function.name
+  role           = var.gcf_role_bind 
+  member         = var.gcf_member_account 
+}
