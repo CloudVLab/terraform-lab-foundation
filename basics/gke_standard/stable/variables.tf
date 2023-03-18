e59c97e3a2dd561617ca2ff54f2ff10c839250f3
rosera <askrichardrose@gmail.com>
2023-03-18 16:47:50 +0000

    Add: Machine Type property

--- basics/gke_standard/stable/variables.tf
+++ basics/gke_standard/stable/variables.tf
@@ -57,6 +57,62 @@ variable "gkeDescription" {
   default     = "Lab cluster - using default description" 
 }
 
+variable "gkeMachineType" {
+  type        = string 
+  description = "Machine Type to use."
+  default     = "e2-standard-2" 
+}
+
+variable "gkeIsPrivateCluster" {
+  type        = bool
+  description = "Set as True to spin up a private, secure cluster. False to spin up a public cluster."
+  default     = true
+}
+
+variable "gkeIsPrivateEndpoint" {
+  type        = bool
+  description = "Set as True use the cluster private endpoint and disable public endpoint. False means cluster cluster/public endpoint can be used."
+  default     = false 
+}
+
+variable "gkeNetworkingMode" {
+  type        = bool 
+  description = "Set to true for VPC_NATIVE (IP aliasing), false for ROUTES (default)"
+  default     = false 
+}
+
+variable "gkeIsAutopilot" {
+  type        = bool
+  description = "Set as True to utilize custom network resources. False to switch to default network."
+  default     = false 
+}
+
+variable "gkeIsBinAuth" {
+  type        = bool 
+  description = "Set as True to enable Binary Authorization" 
+  default     = false 
+}
+
+variable "gkeInitialNodeCount" {
+  type        = string 
+  description = "Set the initial number of nodes" 
+  default     = "1"
+}
+
+# Custom properties with defaults 
+variable "gkeMasterIPv4CIDRBlock" {
+  type        = string
+  description = "GKE Master CIDR block"
+  default     = "172.23.0.0/28"
+}
+
+# Custom properties with defaults 
+variable "gkeModeVpcNative" {
+  type        = string
+  description = "Networking mode - VPC Native"
+  default     = "VPC_NATIVE"
+}
+
 variable "gkeIsPrivateCluster" {
   type        = bool
   description = "Set as True to spin up a private, secure cluster. False to spin up a public cluster."
