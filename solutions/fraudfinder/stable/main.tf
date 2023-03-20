ebf68ddde68b3f433d2d10e50b825da6b259b330
Polong Lin <polong-lin@users.noreply.github.com>
2023-03-20 11:31:22 +0000

    commit before testing

--- solutions/fraudfinder/stable/main.tf
+++ solutions/fraudfinder/stable/main.tf
@@ -7,6 +7,7 @@ data "google_project" "project" {}
 /*
   Enable Required APIs
 */
+
 variable "gcp_service_list" {
   description = "The list of apis necessary for the project"
   type        = list(string)
@@ -44,10 +45,14 @@ resource "random_string" "rand" {
   upper   = false
 }
 
+/*
+  Set local variables
+*/
+
 locals {
-  ID                            = random_string.rand.result
-  NOTEBOOK_LOG                  = "/tmp/notebook_config.log"
-  compute_service_account_email = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  ID                                = random_string.rand.result
+  NOTEBOOK_LOG                      = "/tmp/notebook_config.log"
+  compute_service_account_email     = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
   cloud_build_service_account_email = "${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
 
@@ -73,6 +78,133 @@ resource "google_pubsub_subscription" "ff-txlabels-subscription" {
   topic   = "projects/cymbal-fraudfinder/topics/ff-txlabels"
 }
 
+/*
+  Create GCP Storage Bucket
+  Set storage admin on bucket to SA and Lab user
+*/
+resource "google_storage_bucket" "fraudfinder_bucket" {
+  name          = "${var.gcp_project_id}-fraudfinder"
+  location      = var.gcp_region
+  force_destroy = true
+}
+
+# resource "google_storage_bucket_iam_binding" "fraudfinder_bucket_iam_binding" {
+#   bucket = google_storage_bucket.fraudfinder_bucket.name
+#   role   = "roles/storage.admin"
+#   members = [
+#     "user:${var.gcp_user_id}",
+#     "serviceAccount:${local.compute_service_account_email}"
+#   ]
+# }
+
+resource "google_storage_bucket" "lab_config_bucket" {
+  name          = "${var.gcp_project_id}-labconfig-bucket"
+  location      = var.gcp_region
+  force_destroy = true
+}
+
+# resource "google_storage_bucket_iam_binding" "lab_config_bucket_iam_binding" {
+#   bucket = google_storage_bucket.lab_config_bucket.name
+#   role   = "roles/storage.admin"
+#   members = [
+#     "user:${var.gcp_user_id}",
+#     "serviceAccount:${local.compute_service_account_email}"
+#   ]
+# }
+
+/*
+  Create config scripts & upload to buckets
+*/
+
+resource "local_file" "notebook_config" {
+
+  content = <<EOF
+echo "Current user: `id`" >> ${local.NOTEBOOK_LOG} 2>&1
+echo "Creating pub/sub subscriptions" >> ${local.NOTEBOOK_LOG} 2>&1
+gcloud pubsub subscriptions create "ff-tx-sub" --topic="ff-tx" --topic-project="cymbal-fraudfinder" >> ${local.NOTEBOOK_LOG} 2>&1
+gcloud pubsub subscriptions create "ff-tx-for-feat-eng-sub" --topic="ff-tx" --topic-project="cymbal-fraudfinder"
+gcloud pubsub subscriptions create "ff-txlabels-sub" --topic="ff-txlabels" --topic-project="cymbal-fraudfinder" >> ${local.NOTEBOOK_LOG} 2>&1
+echo "Changing dir to /home/jupyter" >> ${local.NOTEBOOK_LOG} 2>&1
+cd /home/jupyter
+echo "Cloning fraudfinder from github" >> ${local.NOTEBOOK_LOG} 2>&1
+su - jupyter -c "git clone https://github.com/GoogleCloudPlatform/fraudfinder.git" >> ${local.NOTEBOOK_LOG} 2>&1
+echo "Current user: `id`" >> ${local.NOTEBOOK_LOG} 2>&1
+echo "Installing python packages" >> ${local.NOTEBOOK_LOG} 2&1
+su - jupyter -c "pip install --upgrade --no-warn-conflicts --no-warn-script-location --user \
+    google-cloud-pubsub==2.13.6 \
+    google-api-core==2.8.2 \
+    google-apitools==0.5.32 \
+    plotly==5.10.0 \
+    itables==1.2.0 \
+    xgboost==1.6.2 \
+    apache_beam==2.40.0 \
+    plotly==5.10.0 \
+    google-cloud-pipeline-components \
+    kfp" >> ${local.NOTEBOOK_LOG} 2>&1
+
+# echo "Executing biqquery data copy script" >> ${local.NOTEBOOK_LOG} 2>&1
+# su - jupyter -c "python /home/jupyter/fraudfinder/scripts/copy_bigquery_data.py ${google_storage_bucket.fraudfinder_bucket.name}" >> ${local.NOTEBOOK_LOG} 2>&1
+
+EOF
+
+  filename = "notebook_config.sh"
+}
+
+resource "google_storage_bucket_object" "notebook_config_script" {
+  name   = "notebook_config.sh"
+  source = "notebook_config.sh"
+  bucket = google_storage_bucket.lab_config_bucket.name
+  depends_on = [
+    google_storage_bucket.lab_config_bucket,
+    local_file.notebook_config
+  ]
+}
+
+# resource "local_file" "config_data" {
+
+#   content = <<EOF
+# BUCKET_NAME          = "${google_storage_bucket.fraudfinder_bucket.name}"
+# PROJECT              = "${var.gcp_project_id}"
+# REGION               = "${var.gcp_region}"
+# ID                   = "${local.ID}"
+# FEATURESTORE_ID      = "fraudfinder_${local.ID}"
+# MODEL_NAME           = "fraudfinder_logreg_model"
+# ENDPOINT_NAME        = "fraudfinder_logreg_endpoint"
+# TRAINING_DS_SIZE     = "1000"
+# EOF
+
+#   filename = "notebook_env.py"
+# }
+
+# resource "google_storage_bucket_object" "notebook_env_file" {
+#   name   = "config/notebook_env.py"
+#   source = "notebook_env.py"
+#   bucket = google_storage_bucket.fraudfinder_bucket.name
+#   depends_on = [
+#     google_storage_bucket.fraudfinder_bucket,
+#     local_file.config_data
+#   ]
+# }
+
+/*
+  Create Vertex AI Notebook
+*/
+
+resource "google_notebooks_instance" "ff_notebook" {
+  name               = "ff-jupyterlab"
+  project            = var.gcp_project_id
+  location           = var.gcp_zone
+  machine_type       = "n1-standard-4" // n1-standard-1 $112.91 monthly estimate
+  install_gpu_driver = false
+  vm_image { // https://cloud.google.com/vertex-ai/docs/workbench/user-managed/images
+    project      = "deeplearning-platform-release"
+    image_family = "common-cpu-notebooks"
+  }
+
+  post_startup_script = "gs://${var.gcp_project_id}-labconfig-bucket/notebook_config.sh"
+  
+  depends_on = [google_project_service.gcp_services, google_storage_bucket_object.notebook_config_script]
+}
 /*
   Assign Appropriate IAM Permissions to the compute SA
 */
@@ -108,12 +240,13 @@ resource "google_project_iam_member" "servacct-compute-add-permissions" {
 /*
   Assign Appropriate IAM Permissions to the Cloud Build SA
 */
-
+        
 variable "cloud_build_service_account_project_iam_list" {
   description = "Roles needed for compute SA"
   type        = list(string)
   default = [
     "roles/run.admin",
+    "roles/cloudbuild.serviceAgent",
     "roles/aiplatform.admin",
     "roles/cloudbuild.builds.editor",
     "roles/cloudbuild.integrations.editor",
@@ -126,156 +259,5 @@ resource "google_project_iam_member" "servacct-cloud-build-add-permissions" {
   project  = var.gcp_project_id
   role     = each.key
   member   = "serviceAccount:${local.cloud_build_service_account_email}"
+  depends_on = [google_notebooks_instance.ff_notebook]
 }
-
-
-/*
-  Create GCP Storage Bucket
-  Set storage admin on bucket to SA and Lab user
-*/
-resource "google_storage_bucket" "fraudfinder_bucket" {
-  name          = "${var.gcp_project_id}-fraudfinder"
-  location      = var.gcp_region
-  force_destroy = true
-}
-
-resource "google_storage_bucket_iam_binding" "fraudfinder_bucket_iam_binding" {
-  bucket = google_storage_bucket.fraudfinder_bucket.name
-  role   = "roles/storage.admin"
-  members = [
-    "user:${var.gcp_user_id}",
-    "serviceAccount:${local.compute_service_account_email}"
-  ]
-}
-
-resource "google_storage_bucket" "lab_config_bucket" {
-  name          = "${var.gcp_project_id}-labconfig-bucket"
-  location      = var.gcp_region
-  force_destroy = true
-}
-
-resource "google_storage_bucket_iam_binding" "lab_config_bucket_iam_binding" {
-  bucket = google_storage_bucket.lab_config_bucket.name
-  role   = "roles/storage.admin"
-  members = [
-    "user:${var.gcp_user_id}",
-    "serviceAccount:${local.compute_service_account_email}"
-  ]
-}
-
-/*
-  Create config scripts & upload to buckets
-*/
-
-resource "local_file" "notebook_config" {
-
-  content = <<EOF
-echo "Current user: `id`" >> ${local.NOTEBOOK_LOG} 2>&1
-echo "Creating pub/sub subscriptions" >> ${local.NOTEBOOK_LOG} 2>&1
-gcloud pubsub subscriptions create "ff-tx-sub" --topic="ff-tx" --topic-project="cymbal-fraudfinder" >> ${local.NOTEBOOK_LOG} 2>&1
-gcloud pubsub subscriptions create "ff-tx-for-feat-eng-sub" --topic="ff-tx" --topic-project="cymbal-fraudfinder"
-gcloud pubsub subscriptions create "ff-txlabels-sub" --topic="ff-txlabels" --topic-project="cymbal-fraudfinder" >> ${local.NOTEBOOK_LOG} 2>&1
-echo "Changing dir to /home/jupyter" >> ${local.NOTEBOOK_LOG} 2>&1
-cd /home/jupyter
-echo "Cloning fraudfinder from github" >> ${local.NOTEBOOK_LOG} 2>&1
-su - jupyter -c "git clone https://github.com/GoogleCloudPlatform/fraudfinder.git" >> ${local.NOTEBOOK_LOG} 2>&1
-echo "Current user: `id`" >> ${local.NOTEBOOK_LOG} 2>&1
-echo "Installing python packages" >> ${local.NOTEBOOK_LOG} 2&1
-su - jupyter -c "pip install --upgrade --no-warn-conflicts --no-warn-script-location --user \
-    google-cloud-pubsub==2.13.6 \
-    google-api-core==2.8.2 \
-    google-apitools==0.5.32 \
-    plotly==5.10.0 \
-    itables==1.2.0 \
-    xgboost==1.6.2 \
-    apache_beam==2.40.0 \
-    plotly==5.10.0 \
-    google-cloud-pipeline-components \
-    kfp" >> ${local.NOTEBOOK_LOG} 2>&1
-
-echo "Executing biqquery data copy script" >> ${local.NOTEBOOK_LOG} 2>&1
-su - jupyter -c "python /home/jupyter/fraudfinder/scripts/copy_bigquery_data.py ${google_storage_bucket.fraudfinder_bucket.name}" >> ${local.NOTEBOOK_LOG} 2>&1
-
-EOF
-
-  filename = "notebook_config.sh"
-}
-
-resource "google_storage_bucket_object" "notebook_config_script" {
-  name   = "notebook_config.sh"
-  source = "notebook_config.sh"
-  bucket = google_storage_bucket.lab_config_bucket.name
-  depends_on = [
-    google_storage_bucket.lab_config_bucket,
-    local_file.notebook_config
-  ]
-}
-
-resource "local_file" "config_data" {
-
-  content = <<EOF
-BUCKET_NAME          = "${google_storage_bucket.fraudfinder_bucket.name}"
-PROJECT              = "${var.gcp_project_id}"
-REGION               = "${var.gcp_region}"
-ID                   = "${local.ID}"
-FEATURESTORE_ID      = "fraudfinder_${local.ID}"
-MODEL_NAME           = "fraudfinder_logreg_model"
-ENDPOINT_NAME        = "fraudfinder_logreg_endpoint"
-TRAINING_DS_SIZE     = "1000"
-EOF
-
-  filename = "notebook_env.py"
-}
-
-resource "google_storage_bucket_object" "notebook_env_file" {
-  name   = "config/notebook_env.py"
-  source = "notebook_env.py"
-  bucket = google_storage_bucket.fraudfinder_bucket.name
-  depends_on = [
-    google_storage_bucket.fraudfinder_bucket,
-    local_file.config_data
-  ]
-}
-
-/*
-  Create Vertex AI Notebook
-*/
-
-resource "google_notebooks_instance" "ff-notebook" {
-  name               = "ff-jupyterlab"
-  project            = var.gcp_project_id
-  location           = var.gcp_zone
-  machine_type       = "n1-standard-4" // n1-standard-1 $112.91 monthly estimate
-  install_gpu_driver = false
-  service_account    = "${local.compute_service_account_email}"
-  vm_image { // https://cloud.google.com/vertex-ai/docs/workbench/user-managed/images
-    project      = "deeplearning-platform-release"
-    image_family = "common-cpu-notebooks"
-  }
-
-  post_startup_script = "gs://${var.gcp_project_id}-labconfig-bucket/notebook_config.sh"
-  
-  depends_on = [google_project_service.gcp_services, google_storage_bucket_object.notebook_config_script]
-}
-
-# resource "google_artifact_registry_repository" "mlops-repo" {
-#   location      = "us-central1"
-#   repository_id = "mlops-repository"
-#   description   = "MLOps Docker Repository"
-#   format        = "DOCKER"
-# }
-
-/*
-  Create PubSub topics for transactions
-*/
-
-# resource "google_pubsub_subscription" "ff_tx-sub" {
-#   name    = "ff-tx-sub"
-#   topic   = "ff-tx"
-# }
-
-# resource "google_pubsub_subscription" "ff_txlabels_sub" {
-#   project = "cymbal-fraudfinder"
-#   name    = "ff-txlabels-sub"
-#   topic   = "ff-txlabels"
-# }
