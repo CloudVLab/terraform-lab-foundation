7685489c377a768cec950d217cc965f607060f94
rosera <askrichardrose@gmail.com>
2023-03-21 19:23:09 +0000

    Add: Invalid Node Count

--- basics/gke_standard/stable/main.tf
+++ basics/gke_standard/stable/main.tf
@@ -17,7 +17,7 @@ resource "google_container_cluster" "tfer-gke" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 1
+  # initial_node_count       = 1
 
   network    = var.gkeNetwork 
   subnetwork = var.gkeSubnetwork 
