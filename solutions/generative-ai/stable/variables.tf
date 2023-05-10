92db8459dd2b3c718077c81799037371529016a0
Polong Lin <polong-lin@users.noreply.github.com>
2023-05-10 09:59:17 +0000

    init generative-ai/ terraform solution

---
+++ solutions/generative-ai/stable/variables.tf
@@ -0,0 +1,25 @@
+## --------------------------------------------------------------
+## Mandatory variable definitions
+## --------------------------------------------------------------
+
+variable "gcp_project_id" {
+  type        = string
+  description = "The GCP project ID to create resources in."
+}
+
+# Default value passed in
+variable "gcp_region" {
+  type        = string
+  description = "Region to create resources in."
+}
+
+# Default value passed in
+variable "gcp_zone" {
+  type        = string
+  description = "Zone to create resources in."
+}
+
+variable "gcp_user_id" {
+  type        = string
+  description = "User Id"
+}
