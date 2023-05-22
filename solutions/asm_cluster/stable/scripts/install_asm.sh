4487b41cf1066e691da4b69fc49bc64bf7e206fb
Vishal Patel <5624846+vishal84@users.noreply.github.com>
2023-05-22 11:11:47 -0400

    fix: added dev to stable

---
+++ solutions/asm_cluster/stable/scripts/install_asm.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Set the Platform Project
+gcloud config set project "$1"
+
+# Create CloudBuild script 
+cat << 'EOF' > cloudbuild.yaml 
+steps:
+- id: startup_script 
+  name: 'gcr.io/google.com/cloudsdktool/cloud-sdk'
+  env:
+  - 'PROJECT_ID=${_PROJECT_ID}'
+  - 'PROJECT_NUMBER=${_PROJECT_NUMBER}'
+  - 'ZONE=${_ZONE}'
+  - 'GCP_USERNAME=${_GCP_USERNAME}'
+  - 'CLUSTER_NAME=${_CLUSTER_NAME}'
+  - 'ASM_VERSION=${_ASM_VERSION}'
+  - 'KUBECONFIG=${_KUBECONFIG}'
+  script: |
+    #!/bin/bash
+
+    apt-get update && apt-get install kubectl jq git netcat -y
+    gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE --project $PROJECT_ID
+    kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$GCP_USERNAME@qwiklabs.net
+    
+    curl -L https://storage.googleapis.com/csm-artifacts/asm/asmcli_1.16 -o asmcli
+    chmod +x asmcli
+    mv asmcli /usr/local/bin/asmcli
+
+    asmcli install \
+    --project_id $PROJECT_ID \
+    --cluster_name $CLUSTER_NAME \
+    --cluster_location $ZONE \
+    --fleet_id $PROJECT_ID \
+    --enable_all \
+    --option legacy-default-ingressgateway \
+    --ca mesh_ca \
+    --enable_gcp_components
+   
+    echo "Enabling Sidecar Injection..."
+    kubectl label namespace default istio-injection=enabled --overwrite
+timeout: 900s
+substitutions:
+  _PROJECT_ID: project_id
+  _PROJECT_NUMBER: project_number
+  _ZONE: zone
+  _GCP_USERNAME: gcp_username
+  _CLUSTER_NAME: cluster_name
+  _ASM_VERSION: '1.16'
+  _KUBECONFIG: ~/.kubeconfig
+options:
+  substitution_option: 'ALLOW_LOOSE'
+EOF
+
+# Initiate CloudBuild Trigger 
+gcloud builds submit --config=cloudbuild.yaml --project="$1" --substitutions=_PROJECT_ID="$1",_PROJECT_NUMBER="$2",_ZONE="$3",_GCP_USERNAME="$4",_CLUSTER_NAME="$5"
--- solutions/asm_cluster/stable/variables.tf
+++ solutions/asm_cluster/stable/variables.tf
@@ -23,135 +23,17 @@ variable "gcp_zone" {
 ## Output variable definitions - Override from Custom Properties 
 ## --------------------------------------------------------------
 
-### Retired - please use gkeClusterName instead
-# Expected custom_properties key to be declared in `qwiklabs.yaml` 
-# with the same name for any lab that uses this script.
-variable "gke_cluster_name" {
-  type        = string
-  description = "GKE Cluster name."
-  default     = "test-cluster"
-}
-
-# with the same name for any lab that uses this script.
 variable "gcp_username" {
   type        = string
-  description = "Name of Qwiklabs user"
-  default     = "student"
+  description = "The lab username"
 }
 
-
-## --------------------------------------------------------------
-## Custom variable definitions - Override from Custom Properties
-## --------------------------------------------------------------
-
-# Custom properties with defaults 
-variable "gkeClusterName" {
-  type        = string
-  description = "GKE Cluster name."
-  default     = "dev-cluster" 
+variable "service_account_key_file" {
+  type = string
+  description = "key file location"
 }
 
-variable "gkeDescription" {
-  type        = string 
-  description = "Description to apply to the cluster."
-  default     = "Lab cluster - using default description" 
-}
-
-variable "gkeIsPrivateCluster" {
-  type        = bool
-  description = "Set as True to spin up a private, secure cluster. False to spin up a public cluster."
-  default     = true
-}
-
-variable "gkeIsPrivateEndpoint" {
-  type        = bool
-  description = "Set as True use the cluster private endpoint and disable public endpoint. False means cluster cluster/public endpoint can be used."
-  default     = false 
-}
-
-variable "gkeNetworkingMode" {
-  type        = bool 
-  description = "Set to true for VPC_NATIVE (IP aliasing), false for ROUTES (default)"
-  default     = false 
-}
-
-variable "gkeIsAutopilot" {
-  type        = bool
-  description = "Set as True to utilize custom network resources. False to switch to default network."
-  default     = false 
-}
-
-variable "gkeIsBinAuth" {
-  type        = bool 
-  description = "Set as True to enable Binary Authorization" 
-  default     = false 
-}
-
-variable "gkeInitialNodeCount" {
-  type        = string 
-  description = "Set the initial number of nodes" 
-  default     = "1"
-}
-
-# Custom properties with defaults 
-variable "gkeMasterIPv4CIDRBlock" {
-  type        = string
-  description = "GKE Master CIDR block"
-  default     = "172.23.0.0/28"
-}
-
-# Custom properties with defaults 
-variable "gkeModeVpcNative" {
-  type        = string
-  description = "Networking mode - VPC Native"
-  default     = "VPC_NATIVE"
-}
-
-# Custom properties with defaults 
-variable "gkeModeRoutes" {
-  type        = string
-  description = "Networking mode - Routes"
-  default     = "ROUTES"
-}
-
-# Custom properties with defaults
-variable "gkeIsCustomNetwork" {
-  type        = bool
-  description = "Set as True to utilize custom network resources. False to switch to default network."
-  default     = false 
-}
-
-# Custom properties with defaults 
-variable "gkeNetwork" {
-  type        = string
-  description = "Network route "
-  default     = "default"
-}
-
-# Custom properties with defaults 
-variable "gkeSubnetwork" {
-  type        = string
-  description = "Network route "
-  default     = "default"
-}
-
-# Custom properties with defaults 
-variable "gkeLabelMeshId" {
-  type        = string
-  description = "Network route "
-  default     = "mesh-12345"
-}
-
-# Custom properties with defaults 
-variable "gkePoolName" {
-  type        = string
-  description = "Network route "
-  default     = "pool-12345"
-}
-
-# Custom properties with defaults 
-variable "gkeWorkloadPool" {
-  type        = string
-  description = "Network route "
-  default     = "pool.svc.id.goog"
+variable "gke_cluster_name" {
+  type = string
+  description = "cluster name for GKE"
 }
