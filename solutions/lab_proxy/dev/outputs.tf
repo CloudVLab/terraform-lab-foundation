94a89049f415a2ff2a0a7471d36ab2bcade33080
rosera <askrichardrose@gmail.com>
2023-07-28 12:23:35 +0100

    Add: Mandatory parameters + formatting

--- solutions/lab_proxy/dev/outputs.tf
+++ solutions/lab_proxy/dev/outputs.tf
@@ -3,6 +3,6 @@
 ## --------------------------------------------------------------
 
 output "ideEditorService" {
-  value       = "${google_cloud_run_service.ide.status[0].url}"
+  value       = google_cloud_run_service.ide.status[0].url
   description = "URL of the IDE service"
 }
