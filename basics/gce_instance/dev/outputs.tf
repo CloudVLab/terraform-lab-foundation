f47aa376a2dea7dc17729d34ea2ef3de329d259e
rosera <askrichardrose@gmail.com>
2024-04-27 09:21:09 +0100

    Add: Fix for invalid variable name

--- basics/gce_instance/dev/outputs.tf
+++ basics/gce_instance/dev/outputs.tf
@@ -8,7 +8,7 @@
 # }
 
 output "gce_external_ip" {
-  value = var.assign_external_ip ? (
+  value = var.gce_assign_external_ip ? (
     google_compute_instance.gce_virtual_machine.network_interface.0.access_config.0.nat_ip
   ) : null
   description = "External IP address of GCE instance"
