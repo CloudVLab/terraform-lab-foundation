761631fb9bf435af049772c5471c928b6b655ad4
rosera <askrichardrose@gmail.com>
2024-05-11 14:52:14 +0100

    May Housekeeping

---
+++ basics/api_service/stable/v1/variables.tf
@@ -0,0 +1,45 @@
+## --------------------------------------------------------------
+## Mandatory variable definitions
+## --------------------------------------------------------------
+
+variable "gcp_project_id" {
+  type        = string
+  description = "The GCP project ID to create resources in."
+}
+
+# Default value passed in
+variable "gcp_region" {
+  type        = string
+  description = "Region to create resources in."
+}
+
+# Default value passed in
+variable "gcp_zone" {
+  type        = string
+  description = "Zone to create resources in."
+}
+
+## --------------------------------------------------------------
+## Output variable definitions - Override from Custom Properties 
+## --------------------------------------------------------------
+
+# Expected custom_properties key to be declared in `qwiklabs.yaml` 
+# with the same name for any lab that uses this script.
+## variable "api_service_name" {
+##   type        = string
+##   description = "Googleapis service name."
+## }
+
+variable "api_services" {
+  type = list(string)
+  default = [
+    "storage-api.googleapis.com",
+  ]
+}
+
+# with the same name for any lab that uses this script.
+variable "api_service_deny" {
+  type        = string
+  description = "Name of Qwiklabs user"
+  default     = false
+}
