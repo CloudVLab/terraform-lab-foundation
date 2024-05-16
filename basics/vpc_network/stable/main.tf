ff9a6f26206b289d680a1fbcd37b032c9a5cdfd5
rosera <askrichardrose@gmail.com>
2024-05-17 00:30:44 +0100

    Add: VPC delay before subnet creation

--- basics/vpc_network/stable/main.tf
+++ basics/vpc_network/stable/main.tf
@@ -8,6 +8,11 @@ resource "google_compute_network" "dev_network" {
   project                 = var.gcp_project_id
 }
 
+resource "time_sleep" "wait_vpc_delay" {
+  create_duration = "60s"
+  depends_on      = [ google_compute_network.dev_network.name ]
+}
+
 # Reference:
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
 resource "google_compute_subnetwork" "dev_subnet" {
@@ -17,9 +22,13 @@ resource "google_compute_subnetwork" "dev_subnet" {
   network       = google_compute_network.dev_network.id
   project       = var.gcp_project_id
   private_ip_google_access = var.vpc_private_google_access
+
 ##   log_config    {
 ##     aggregation_interval = "INTERVAL_10_MIN"
 ##     flow_sampling        = 0.5
 ##     metadata             = "INCLUDE_ALL_METADATA"
 ##   }
+
+  # Give network time to be created
+  depends_on = [ time_sleep.wait_vpc_delay ]
 }
