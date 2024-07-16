af8a96ec28c67f4ef48bcd4e1f58bdd6b0e4b19f
rosera <askrichardrose@gmail.com>
2024-07-16 16:35:48 +0100

    Add: Migrate source to use Cloud Storage

--- basics/firebase_project/example/main.tf
+++ basics/firebase_project/example/main.tf
@@ -4,7 +4,8 @@
 
 # Module: Google Compute Engine
 module "la_firebase_project" {
-  source = "github.com/CloudVLab/terraform-lab-foundation//basics/firebase_project/stable"
+  # source = "github.com/CloudVLab/terraform-lab-foundation//basics/firebase_project/stable"
+  source = "gcs::https://www.googleapis.com/storage/v1/terraform-lab-foundation/basics/firebase_project/stable/v1"
 
   # Pass values to the module
   gcp_project_id = var.gcp_project_id
