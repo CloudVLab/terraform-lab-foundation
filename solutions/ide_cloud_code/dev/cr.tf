1765af7129e3fc58deb30a4491b4c651ff9a5147
rosera <askrichardrose@gmail.com>
2024-09-10 11:46:24 +0100

    Add: module to replace resource

--- solutions/ide_cloud_code/dev/cr.tf
+++ solutions/ide_cloud_code/dev/cr.tf
@@ -1,35 +1,35 @@
-# TODO: Migrate to Module - Cloud Run V2 
+# Module: Cloud Run Gen v2 
+module "la_cloud_run" {
+  #source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_run/stable?ref=tlf-basics"
+  # source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_run/stable/v1"
+  # source = "gcs::https://storage.googleapis.com/terraform-lab-foundation/basics/cloud_run/stable"
+  # source = "github.com/CloudVLab/terraform-lab-foundation//basics/cloud_run/stable"
+  source = "gcs::https://www.googleapis.com/storage/v1/terraform-lab-foundation/basics/cloud_run/stable/v2"
 
+  # Pass values to the module
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = var.gcp_region
+  gcp_zone       = var.gcp_zone
 
-## Create a Cloud Run container 
-## ----------------------------------------------------------------------------
-
-resource "google_cloud_run_v2_service" "ide" {
-  name     = var.gcrIDEService 
-  location = var.gcp_region 
-  launch_stage = "BETA"
-  template {
-    containers {
-      image = "gcr.io/qwiklabs-resources/test-ide-proxy:latest"
-    }
-    vpc_access{
-      network_interfaces {
-        network = module.la_vpc.vpc_network_name
-        subnetwork = module.la_vpc.vpc_subnetwork_name
-        # network = google_compute_network.dev_network.id 
-        # subnetwork = google_compute_subnetwork.dev_subnet.id
-        tags = var.gceInstanceTags
-      }
-      egress = "ALL_TRAFFIC"
-    }
-  }
+  # Customise the service 
+  gcrServiceName  = "ide-service"
+  gcrServiceImage = "gcr.io/qwiklabs-resources/ide-proxy-v1:latest"
+  gcrNetworkName  = module.la_vpc.vpc_network_name
+  gcrSubnetName   = module.la_vpc.vpc_subnetwork_name
 }
 
-resource "google_cloud_run_service_iam_binding" "public" {
-  location = google_cloud_run_v2_service.ide.location
-  service  = google_cloud_run_v2_service.ide.name
-  role     = "roles/run.invoker"
-  members  = [
-    "allUsers"
-  ]
+# Module: Cloud Run Gen v2 - BROWSER
+module "la_cloud_run_browser" {
+  source = "gcs::https://www.googleapis.com/storage/v1/terraform-lab-foundation/basics/cloud_run/stable/v2"
+
+  # Pass values to the module
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = var.gcp_region
+  gcp_zone       = var.gcp_zone
+
+  # Customise the service
+  gcrServiceName  = "browser-service"
+  gcrServiceImage = "gcr.io/qwiklabs-resources/web-proxy-v1:latest"
+  gcrNetworkName  = module.la_vpc.vpc_network_name
+  gcrSubnetName   = module.la_vpc.vpc_subnetwork_name
 }
