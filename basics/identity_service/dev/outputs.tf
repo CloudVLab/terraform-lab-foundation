cd6ff958f7db3d1d2a019d790c8a5060837d19e6
rosera <askrichardrose@gmail.com>
2024-10-08 16:33:48 +0100

    Add: Map for each set item

--- basics/identity_service/dev/outputs.tf
+++ basics/identity_service/dev/outputs.tf
@@ -6,7 +6,12 @@ output "wait_identity_delay" {
   description = "Custom delay for API to be enabled"
 }
 
-output "service_account_member" {
-  value = google_project_service_identity.tlf[tolist(google_project_service_identity.tlf)[0]].member
-  description = "Service identity member service account"
+output "service_account_emails" {
+  value = { for k, v in google_project_service_identity.tlf : k => v.member }
+  description = "Map of service names to their corresponding service account emails"
 }
+
+## output "service_account_member" {
+##   value = google_project_service_identity.tlf[tolist(google_project_service_identity.tlf)[0]].member
+##   description = "Service identity member service account"
+## }
