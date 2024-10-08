edc6af8118ef1d5d0e30091697595eb4d8e2ccb2
rosera <askrichardrose@gmail.com>
2024-10-08 16:11:20 +0100

    Add set to list conversion

--- basics/identity_service/dev/outputs.tf
+++ basics/identity_service/dev/outputs.tf
@@ -3,8 +3,10 @@
 ## --------------------------------------------------------------
 output "wait_identity_delay" {
   value = time_sleep.wait_identity_delay.id
+  description = "Custom delay for API to be enabled"
 }
 
 output "service_account_member" {
-  value = google_project_service_identity.tlf[0].member
+  value = tolist(google_project_service_identity.tlf)[0].member
+  description = "Service identity member service account"
 }
