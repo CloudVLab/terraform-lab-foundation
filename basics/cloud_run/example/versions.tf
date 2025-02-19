f140e8c05dcbd974db7ca329a513b91394661c6b
Daniel Gorelik <dgorelik@google.com>
2025-02-19 17:36:54 -0500

    Revert "Add: google-beta fix - Basics"

--- basics/cloud_run/example/versions.tf
+++ basics/cloud_run/example/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       # https://registry.terraform.io/providers/hashicorp/google/latest
-      version = ">=5.6.0"
+      version = "5.6.0"
     }
-##    google-beta = {
-##      # https://registry.terraform.io/providers/hashicorp/google-beta/latest
-##      version = "5.6.0"
-##    }
+    google-beta = {
+      # https://registry.terraform.io/providers/hashicorp/google-beta/latest
+      version = "5.6.0"
+    }
   }
 }
--- basics/gke_cluster/dev/provider.tf
+++ basics/gke_cluster/dev/provider.tf
@@ -4,8 +4,8 @@ provider "google" {
   zone    = var.gcp_zone
 }
 
-## provider "google-beta" {
-##   project = var.gcp_project_id
-##   region  = var.gcp_region
-##   zone    = var.gcp_zone
-## }
+provider "google-beta" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+}
