63f2f191fe12b9ec70f22c625919834af0fd260e
rosera <askrichardrose@gmail.com>
2025-07-31 11:41:55 +0000

    Add: CIDR output variable for subnet

--- basics/vpc_subnet/beta/v1/outputs.tf
+++ basics/vpc_subnet/beta/v1/outputs.tf
@@ -11,3 +11,8 @@ output "vpc_subnet_self_link" {
   value       = "${google_compute_subnetwork.dev_subnet.self_link}"
   description = "Self Link to the VPC subnet"
 }
+
+output "vpc_subnet_cidr_range" {
+  value       = google_compute_subnetwork.dev_subnet.ip_cidr_range
+  description = "CIDR range"
+}
