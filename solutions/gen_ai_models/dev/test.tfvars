d364fc4055caf84a1aff5ff36fbd0490da9e5957
rocky lubbers <rlubbers@google.com>
2025-08-28 11:53:00 -0700

    remove variables files

--- solutions/gen_ai_models/dev/test.tfvars
+++
@@ -1,3 +0,0 @@
-gcp_project_id = "qwiklabs-resources"
-gcp_region     = "us-central1"
-gcp_zone       = "us-central1-a"
--- solutions/gen_ai_models/example/main.tf
+++ solutions/gen_ai_models/example/main.tf
@@ -3,8 +3,8 @@
 #
 
 # Solution: FraudFinder environment 
-# Local:  solutions/fraudfinder/stable
-# Remote: github//solutions/fraudfinder/stable
+# Local:  solutions/gen_ai_models/stable
+# Remote: github//solutions/gen_ai_models/stable
 
 module "gen_ai_models" {
   ## NOTE: When changing the `source` parameter
@@ -12,10 +12,4 @@ module "gen_ai_models" {
 
   ## REMOTE: GitHub (Public) access - working 
   source = "github.com/CloudVLab/terraform-lab-foundation//solutions/gen_ai_models/stable"
-
-  ## Exchange values between Qwiklabs and Module
-  gcp_project_id = var.gcp_project_id
-  gcp_region     = var.gcp_region
-  gcp_zone       = var.gcp_zone
-  gcp_user_id    = var.gcp_user_id
 }
