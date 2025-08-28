76d2a1b9d355db89d950fd9be5904b28f8697cb8
rocky lubbers <rlubbers@google.com>
2025-08-28 10:52:37 -0700

    test updating models to 2.0 versions

--- solutions/gen_ai_models/main.tf
+++ solutions/gen_ai_models/main.tf
@@ -2,12 +2,12 @@
 locals {
   models = {
     "gemini_pro" = {
-      "model_id"   = "gemini-2.5-pro"
-      "model_name" = "Gemini 2.5 Pro"
+      "model_id"   = "gemini-2.0-pro"
+      "model_name" = "Gemini 2.0 Pro"
     },
     "gemini_flash" = {
-      "model_id"   = "gemini-2.5-flash"
-      "model_name" = "Gemini 2.5 Flash"
+      "model_id"   = "gemini-2.0-flash"
+      "model_name" = "Gemini 2.0 Flash"
     },
     "embedding" = {
       "model_id"   = "gemini-embedding-001"
