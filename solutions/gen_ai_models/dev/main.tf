7c63d6fdce267a639749078da28c0844d90d7158
rocky lubbers <rlubbers@google.com>
2025-09-24 13:49:08 -0700

    add `gemini-2.5-flash-lite` + `multimodalembedding` models

--- solutions/gen_ai_models/dev/main.tf
+++ solutions/gen_ai_models/dev/main.tf
@@ -12,6 +12,14 @@ locals {
     "gemini_embedding" = {
       "model_id"   = "gemini-embedding-001"
       "model_name" = "Gemini Embedding 001"
+    },
+    "gemini_flash_lite" = {
+      "model_id"   = "gemini-2.5-flash-lite"
+      "model_name" = "Gemini 2.5 Flash-Lite"
+    },
+    "multimodal_embedding" = {
+      "model_id"   = "multimodalembedding@001"
+      "model_name" = "Embeddings for Multimodal"
     }
   }
 }
