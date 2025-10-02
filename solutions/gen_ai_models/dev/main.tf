974d4127716c7abda629e8ed28933e0030520d33
rocky lubbers <rlubbers@google.com>
2025-10-02 09:47:13 -0700

    add GA `gemini-2.5-flash-image` model

--- solutions/gen_ai_models/dev/main.tf
+++ solutions/gen_ai_models/dev/main.tf
@@ -9,13 +9,17 @@ locals {
       "model_id"   = "gemini-2.5-flash"
       "model_name" = "Gemini 2.5 Flash"
     },
-    "gemini_embedding" = {
-      "model_id"   = "gemini-embedding-001"
-      "model_name" = "Gemini Embedding 001"
-    },
     "gemini_flash_lite" = {
       "model_id"   = "gemini-2.5-flash-lite"
       "model_name" = "Gemini 2.5 Flash-Lite"
+    },
+    "gemini_flash_image" = {
+      "model_id"   = "gemini-2.5-flash-image"
+      "model_name" = "Gemini 2.5 Flash Image"
+    },
+    "gemini_embedding" = {
+      "model_id"   = "gemini-embedding-001"
+      "model_name" = "Gemini Embedding 001"
     },
     "multimodal_embedding" = {
       "model_id"   = "multimodalembedding@001"
