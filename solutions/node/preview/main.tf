1bfd6bcbbe703aba0a8a780a29989d510b5c74d8
Taylor Levits <taylorlevits@google.com>
2026-07-13 14:16:04 -0600

    add additional data structures

--- solutions/node/preview/main.tf
+++ solutions/node/preview/main.tf
@@ -4,27 +4,44 @@ locals {
     lts = {
       name    = "Node.js LTS"
       version = "lts"
-      command = "nvm install --lts"
+      nvm_command = "nvm install --lts"
+      docker_command = null
+      gcp_runtime = null
     }
     v24 = {
       name    = "Node.js 24"
       version = "24"
-      command = "nvm install 24"
+      nvm_command = "nvm install 24"
+      docker_command = null
+      gcp_runtime = null  
     }
     v22_14_0 = {
       name    = "Node.js 22.14.0"
       version = "22.14.0"
-      command = "nvm install 22.14.0"
+      nvm_command = "nvm install 22.14.0"
+      docker_command = null
+      gcp_runtime = null  
     }
     v22 = {
       name    = "Node.js 22"
       version = "22"
-      command = "nvm install 22"
-    }
+      nvm_command = "nvm install 22"
+      docker_command = null
+      gcp_runtime = null  
+      }
     v20 = {
       name    = "Node.js 20"
       version = "20"
-      command = "nvm install 20"
+      nvm_command = "nvm install 20"
+      docker_command = null
+      gcp_runtime = "nodejs20"
+    },
+    v6_9_2 = {
+      name    = "Node.js 6.9.2"
+      version = "6.9.2"
+      nvm_command = "nvm install 6.9.2"
+      docker_command = "FROM node:6.9.2"
+      gcp_runtime = null  
     }
   }
 }
