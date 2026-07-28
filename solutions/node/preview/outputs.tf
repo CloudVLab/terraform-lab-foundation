15916e0451470f9acc245fc9c28ba5bf5cd4a3ba
rocky lubbers <rlubbers@google.com>
2026-07-28 14:55:41 -0700

    feat(node): refactor Node DDM module to Option A single object pattern

--- solutions/node/preview/outputs.tf
+++ solutions/node/preview/outputs.tf
@@ -1,6 +1,6 @@
 # solutions/node/preview/outputs.tf
 
 output "node" {
-  description = "A map of all available node details."
+  description = "Node.js runtime details for this channel."
   value       = local.node
 }
